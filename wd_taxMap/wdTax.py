db6f4b89000a95710c73ed82d13b30a1e75b3f6f
mdrishti <49712124+mdrishti@users.noreply.github.com>
2025-05-16 21:40:12 +0200

    added duplicate mapping

--- wd_taxMap/wdTax.py
+++ wd_taxMap/wdTax.py
@@ -48,8 +48,8 @@ SELECT ?WdID ?WdName ?hTax ?hTaxName ?hTaxRank WHERE {
 today_str = datetime.today().strftime('%Y%m%d') #date suffix
 
 
-# explore mapping of wikidata to other taxonomic databases
-"""file_mapping = f"wdTax_SPARQL_{today_str}.txt"
+# explore mapping of wikidata to 15 other taxonomic databases and write to a file
+file_mapping = f"wdTax_SPARQL_{today_str}.txt"
 df_mapping = utils.querki_to_dataframe(query_mapping)
 df_mapping.to_csv(file_mapping, index=False)
 file_mappingC = utils.compress_and_remove(file_mapping)
@@ -58,15 +58,16 @@ file_mappingC = utils.compress_and_remove(file_mapping)
 file_lineage = f"wdTax_SPARQL_lineage_{today_str}.txt"
 utils.querki_write_file(query_lineage, file_lineage)
 file_lineageC = utils.compress_and_remove(file_lineage)
-"""
 
-chunk_size = 22787142
-predefined_ranks = ["http://www.wikidata.org/entity/Q35409", "http://www.wikidata.org/entity/Q34740", "http://www.wikidata.org/entity/Q36602", "http://www.wikidata.org/entity/Q38348", "http://www.wikidata.org/entity/Q37517", "http://www.wikidata.org/entity/Q36732", "http://www.wikidata.org/entity/Q7432"]
+
+chunk_size = 22787142 # keep full for getting all taxonomy
+#chunk_size = 30000
+predefined_ranks = ["http://www.wikidata.org/entity/Q36732", "http://www.wikidata.org/entity/Q38348", "http://www.wikidata.org/entity/Q37517", "http://www.wikidata.org/entity/Q36602", "http://www.wikidata.org/entity/Q35409", "http://www.wikidata.org/entity/Q34740", "http://www.wikidata.org/entity/Q7432"]
 
 file_lineage_filtered = f"wdTax_SPARQL_lineage_filtered_{today_str}.txt.gz"
 
 # extract only those rows, which have the predefined ranks
-"""first_chunk = True
+first_chunk = True
 for chunk in pd.read_csv(file_lineageC, chunksize=chunk_size):
     required_columns = ["WdID", "WdName", "hTaxRank", "hTaxName"]
     print(chunk.columns)
@@ -78,37 +79,13 @@ for chunk in pd.read_csv(file_lineageC, chunksize=chunk_size):
     header = first_chunk
     filtered_chunk.to_csv(file_lineage_filtered, compression="gzip", mode=mode, header=header, index=False)
     first_chunk = False
-"""
+
 
 file_lineage_filtered_aligned = f"wdTax_SPARQL_lineage_filtered_aligned_{today_str}.txt.gz"
 
 # operate on the filtered file to align data into columns with ranks as their header
 chunk_iter = pd.read_csv(file_lineage_filtered, compression="gzip", chunksize=chunk_size)
 first_chunk = True
-"""for chunk in chunk_iter:
-    chunk.columns = chunk.columns.str.strip()
-    required_columns = ["WdID", "WdName", "hTaxRank", "hTaxName"]
-    missing_columns = [col for col in required_columns if col not in chunk.columns]
-    if missing_columns:
-        print(f"Error: Missing columns in chunk: {', '.join(missing_columns)}")
-        continue
-    transformed_chunk = chunk[["WdID", "WdName"]].drop_duplicates().set_index(["WdID", "WdName"])
-    for rank in predefined_ranks:
-        transformed_chunk[rank] = ""
-    # manually map values from hTaxRank to predefined columns
-    for _, row in chunk.iterrows():
-        if row["hTaxRank"] in predefined_ranks:
-            transformed_chunk.at[(row["WdID"], row["WdName"]), row["hTaxRank"]] = row["hTaxName"]
-    # reset index to flatten DataFrame
-    transformed_chunk.reset_index(inplace=True)
-    # write to file (header only for first chunk)
-    mode = 'w' if first_chunk else 'a'
-    header = first_chunk
-    transformed_chunk.to_csv(file_lineage_filtered_aligned, compression="gzip", mode=mode, header=header, index=False)
-    # after first write, switch to append mode
-    first_chunk = False
-"""
-
 seen_keys = set()
 duplicate_rows = []
 
@@ -119,50 +96,25 @@ for chunk in chunk_iter:
     if missing_columns:
         print(f"Error: Missing columns in chunk: {', '.join(missing_columns)}")
         continue
-
     transformed_chunk = chunk[["WdID", "WdName"]].drop_duplicates().set_index(["WdID", "WdName"])
     for rank in predefined_ranks:
         transformed_chunk[rank] = ""
-
     # manually map values from hTaxRank to predefined columns
     for _, row in chunk.iterrows():
         if row["hTaxRank"] in predefined_ranks:
             transformed_chunk.at[(row["WdID"], row["WdName"]), row["hTaxRank"]] = row["hTaxName"]
-
     transformed_chunk.reset_index(inplace=True)
+    print(transformed_chunk)
     mode = 'w' if first_chunk else 'a' #write to file (header only for first chunk)
-    header = first_chunk
-#    transformed_chunk.to_csv(file_lineage_filtered_aligned, compression="gzip", mode=mode, header=header, index=False)
+    #header = first_chunk
+    header = ["WdID","WdName","kingdom","phylum","class","order","family","genus","species"]
+    transformed_chunk.to_csv(file_lineage_filtered_aligned, compression="gzip", mode=mode, header=header, index=False) 
     first_chunk = False # after first write, switch to append mode
 
-    # precompute keys as tuples
-    index_tuples = list(zip(transformed_chunk["WdID"], transformed_chunk["WdName"]))
-    current_keys = set(index_tuples)
-
-    # identify duplicates and new keys
-    duplicates = current_keys & seen_keys
-    new_keys = current_keys - seen_keys
-
-    # mask rows
-    is_duplicate = [key in duplicates for key in index_tuples]
-    is_new = [key in new_keys for key in index_tuples]
-
-    dup_df = transformed_chunk[is_duplicate]
-    #write_chunk = transformed_chunk[is_new]
-
-    if not dup_df.empty:
-        duplicate_rows.append(dup_df)
-
-    seen_keys.update(new_keys)
-
-
-
+# detect duplicates in the lineage_aligned file
 file_duplicates = f"wdTax_duplicates_{today_str}.txt"
-if duplicate_rows:
-    all_duplicates = pd.concat(duplicate_rows)
-    # Group by WdID and WdName to sort them together
-    all_duplicates.sort_values(by="WdName", inplace=True)
-    all_duplicates.to_csv("duplicates_output.csv", index=False)
-#if duplicate_rows:
-#    pd.concat(duplicate_rows).to_csv(file_duplicates, index=False)
+header = ["WdID","WdName","kingdom","phylum","class","order","family","genus","species"]
+dup_df = transformed_chunk[transformed_chunk["WdName"].duplicated(keep=False)]
+dup_df.sort_values(by="WdName", inplace=True) # sort so that same WdNames are grouped together
+dup_df.to_csv(file_duplicates, header = header, index=False)
 
